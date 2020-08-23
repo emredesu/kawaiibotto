b393766b84fb6306aad022d23a04a11c29390ea8
emredesu <ahmetemrebenek@gmail.com>
2020-08-23 16:09:36 +0300

    Add files via upload

--- instantiatecommands.py
+++ instantiatecommands.py
@@ -2,13 +2,14 @@ from commands.helloworld import HelloWorldCommand
 from commands.pyramid import PyramidCommand
 from commands.ivrfi_api_commands import RandomQuoteCommand, EmoteInfoCommand
 from commands.opgg import OpggCommand
-from commands.twitch_api_commands import UserIDCommand, EmotesCommand
+from commands.twitch_api_commands import UserIDCommand, ProfilePictureCommand, EmotesCommand
 from commands.query import QueryCommand
 from commands.ping import PingCommand
 from commands.sourcecode import SourceCodeCommand
 from commands.commands import CommandsCommand
 from commands.help import HelpCommand
 from commands.code import CodeCommand
+from commands.math import MathCommand
 
 
 def instantiate_commands(commands):
@@ -25,3 +26,5 @@ def instantiate_commands(commands):
 	CommandsCommand(commands)
 	HelpCommand(commands)
 	CodeCommand(commands)
+	ProfilePictureCommand(commands)
+	MathCommand(commands)
