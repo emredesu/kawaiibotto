ec0e70b5a1630f283ef9c3078908c1c4488e5be5
emredesu <ahmetemrebenek@gmail.com>
2020-08-27 14:51:45 +0300

    better handling for korean users

--- commands/opgg.py
+++ commands/opgg.py
@@ -4,12 +4,12 @@ from commands.command import Command
 class OpggCommand(Command):
 	COMMAND_NAME = "opgg"
 	COOLDOWN = 5
-	DESCRIPTION = "Get OP.GG link of a user. KR server is assumed by op.gg if no regional info is provided. Example usage: _opgg na BUZZLIGHTYEAR99 / _opgg hide on bush (korea)"
+	DESCRIPTION = "Get OP.GG link of a user. Example usage: _opgg euw emredesu"
 
 	def execute(self, bot, user, message, channel):
 		args = message.split()
 
-		available_regions = ["euw", "eune", "jp", "na", "oce", "br", "las", "lan", "ru", "tr", "sg", "id", "ph", "tw", "vn", "th"]
+		available_regions = ["euw", "eune", "jp", "na", "oce", "br", "las", "lan", "ru", "tr", "sg", "id", "ph", "tw", "vn", "th", "kr"]
 
 		try:
 			region = args[1]
@@ -17,11 +17,11 @@ class OpggCommand(Command):
 			bot.send_message(channel, f"Usage: _{self.COMMAND_NAME} (optional region) (player)")
 			return
 		else:
-			if region not in available_regions:
-				player_name = "+".join(args[1::])
+			player_name = "+".join(args[2::])
 
+			if region not in available_regions:
+				bot.send_message(channel, f"Invalid region provided! Available regions are: {' '.join(available_regions)}")
+			elif region == "kr":
 				bot.send_message(channel, f"https://www.op.gg/summoner/userName={player_name}")
 			else:
-				player_name = "+".join(args[2::])
-
 				bot.send_message(channel, f"https://{region}.op.gg/summoner/userName={player_name}")
