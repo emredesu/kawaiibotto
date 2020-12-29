c2db5f4bef69e7a8162160fc2fff9c4f9ba5230f
emredesu <ahmetemrebenek@gmail.com>
2020-12-30 00:34:02 +0300

    colorama is no longer a must-have for the bot to run

--- messagetypes.py
+++ messagetypes.py
@@ -1,7 +1,14 @@
-import colorama
+from importlib import util
 from datetime import datetime
 
-colorama.init()
+has_colorama = None
+
+try:
+	import colorama
+	has_colorama = True
+	colorama.init()
+except ModuleNotFoundError:
+	has_colorama = False
 
 
 def time_now():
@@ -10,12 +17,21 @@ def time_now():
 
 
 def error(string):
-	print(colorama.Fore.RED + ("{} [ERROR] ".format(time_now()) + string))
+	if has_colorama:
+		print(colorama.Fore.RED + ("{} [ERROR] ".format(time_now()) + string))
+	else:
+		print("{} [ERROR] ".format(time_now()) + string)
 
 
 def success(string):
-	print(colorama.Fore.GREEN + ("{} [SUCCESS] ".format(time_now()) + string))
+	if has_colorama:
+		print(colorama.Fore.GREEN + ("{} [SUCCESS] ".format(time_now()) + string))
+	else:
+		print("{} [SUCCESS] ".format(time_now()) + string)
 
 
 def log(string):
-	print(colorama.Fore.BLUE + ("{} [LOG] ".format(time_now()) + string))
+	if has_colorama:
+		print(colorama.Fore.BLUE + ("{} [LOG] ".format(time_now()) + string))
+	else:
+		print("{} [LOG] ".format(time_now()) + string)
