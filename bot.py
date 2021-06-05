37ca0de89a21bebca5d2d2935b57669cfd7f1887
emredesu <ahmetemrebenek@gmail.com>
2021-06-05 20:05:48 +0300

    code cleanup, removed global variables and static methods

--- bot.py
+++ bot.py
@@ -7,134 +7,98 @@ import re
 import time
 import traceback
 
-s = socket.socket()
-connected_once = False
-reconnections = 0
-
-
 class kawaiibotto:
 	def __init__(self):
-		self.connect()
 		self.start_time = datetime.datetime.now()
 		self.commands = []
-		instantiate_commands(self.commands)
+		self.socket = socket.socket()
+		self.connected_once = False
+		self.reconnections = 0
 
-	@staticmethod
-	def send_message(ch, msg):
+		instantiate_commands(self.commands)
+		self.start()
+
+	def send_message(self, ch, msg):
 		if len(msg) > 500:  # can't send messages with a length of over 500 to Twitch IRC, so the bot sends them seperately if the message is larger than 500 characters
 			messages = [msg[i:i+500] for i in range(0, len(msg), 500)]
 			for i in messages:
-				s.send("PRIVMSG #{} :{}\r\n".format(ch, i).encode("utf-8"))
+				self.socket.send("PRIVMSG #{} :{}\r\n".format(ch, i).encode("utf-8"))
 		else:
-			s.send("PRIVMSG #{} :{}\r\n".format(ch, msg.replace("\n", " ")).encode("utf-8"))  # irc does not accept newlines, so we replace them with spaces
+			self.socket.send("PRIVMSG #{} :{}\r\n".format(ch, msg.replace("\n", " ")).encode("utf-8"))  # irc does not accept newlines, so we replace them with spaces
 
 	def connect(self):
-		global s, connected_once
 		while True:
 			try:
-				s.connect((HOST, PORT))
+				self.socket.connect((HOST, PORT))
 			except (socket.gaierror, socket.timeout):
-				error("couldn't connect to {}. retrying in {} seconds...".format(HOST, reconnections ** 2))
-				s.close()
-				s = socket.socket()
-				time.sleep(reconnections ** 2)
+				error("couldn't connect to {}. retrying in {} seconds...".format(HOST, self.reconnections ** 2))
+				self.socket.close()
+				self.socket = socket.socket()
+				time.sleep(self.reconnections ** 2)
 			else:
-				s.send("PASS oauth:{}\r\n".format(OAUTH_TOKEN).encode("utf-8"))
-				s.send("NICK {}\r\n".format(USERNAME).encode("utf-8"))
+				self.socket.send("PASS oauth:{}\r\n".format(OAUTH_TOKEN).encode("utf-8"))
+				self.socket.send("NICK {}\r\n".format(USERNAME).encode("utf-8"))
 				success("connected to twitch.")
 
 				for ch in channels:
-					s.send("JOIN #{}\r\n".format(ch).encode("utf-8"))
-					if not connected_once:
+					self.socket.send("JOIN #{}\r\n".format(ch).encode("utf-8"))
+					if not self.connected_once:
 						log("joined {}".format(ch))
 
-				if not connected_once:
+				if not self.connected_once:
 					self.send_message(debug_channel, "/me {} live with {} {} connected! VoHiYo".format(USERNAME, len(channels), "channel" if len(channels) == 1 else "channels"))
 					connected_once = True
 
 				break
 
-	@staticmethod
-	def reconnect():
-		global s, reconnections
-		reconnections += 1
-		wait_time = 2 ** reconnections
+	def reconnect(self):
+		self.reconnections += 1
+		wait_time = 2 ** self.reconnections
 		if wait_time > 1800:
 			wait_time = 1800
 
 		error("Attempting reconnection in {} seconds...".format(wait_time))
 
-		s.close()
-		s = socket.socket()
+		self.socket.close()
+		self.socket = socket.socket()
 
 		time.sleep(wait_time)
 
-		bot.connect()
-		reconnections = 0
+		self.connect()
+		self.reconnections = 0
 	
-	@staticmethod
-	def parsemsg(s):
+	def parsemsg(self, msg):
 		"""
 		Breaks a message from an IRC server into its prefix, command, and arguments.
 		"""
 		prefix = ''
 		trailing = []
 		try:
-			if s[0] == ':':
-				prefix, s = s[1:].split(' ', 1)
-			if s.find(' :') != -1:
-				s, trailing = s.split(' :', 1)
-				args = s.split()
+			if msg[0] == ':':
+				prefix, msg = msg[1:].split(' ', 1)
+			if msg.find(' :') != -1:
+				msg, trailing = msg.split(' :', 1)
+				args = msg.split()
 				args.append(trailing)
 			else:
-				args = s.split()
+				args = msg.split()
 			command = args.pop(0)
 			return prefix, command, args
 		except Exception as e:
-			error(f"Error while parsing message {s}: {str(e.__class__.__name__)}: {str(e)}")
+			error(f"Error while parsing message {msg}: {e.__class__.__name__}: {str(e)}")
 			return None
 
-	@staticmethod
-	def execute_command(cmnd):
-		if time.time() - cmnd.last_used > cmnd.COOLDOWN:  # cooldown management
-			try:
-				cmnd.execute(bot, user, message, channel)
-				log(f"{user} used {COMMAND_PREFIX}{invoked_command} in {channel}")
-				cmnd.last_used = time.time()
-			except Exception as e:
-				error(f"execution of command {cmnd.COMMAND_NAME} failed with {str(e.__class__.__name__)}: {str(e)}")
-				bot.send_message(channel, f"{user}, the execution of that command failed! Sorry for the inconvenience, it will be fixed soon hopefully ;w;")
-		else:
-			bot.send_message(channel, f"{user}, that command is on cooldown :c")
-
-
-if __name__ == "__main__":
-	bot = kawaiibotto()
-
-	while True:
-		try:
-			response = s.recv(2048).decode("utf-8", "ignore")
-			if not response:
-				error("Disconnected from Twitch. Reconnecting...")
-				bot.reconnect()
-		except Exception as e:
-			error(f"Failed to recv from Twitch, exception: {e.__class__.__name__}")
-			error(f"Printing traceback: ")
-			traceback.print_exc()
-
-			log("Attempting reconnection.")
-			bot.reconnect()
-			continue
-
-		data = bot.parsemsg(response)
+	def process_irc_message(self, message):
+		data = self.parsemsg(message)
 		data_type = data[1] if data is not None else "FAILED_TO_PARSE"
 
 		if data_type == "PING":
-			s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
+			self.socket.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
 			log("answered the twitch ping")
 		elif data_type == "RECONNECT":
-			time.sleep(60)
-			bot.reconnect()
+			log("Reconnecting per Twitch's demand...")
+			self.reconnect()
+			return
 		elif data_type == "PRIVMSG":
 			channel = data[2][0][1::]
 			message = data[2][1]
@@ -149,11 +113,49 @@ if __name__ == "__main__":
 			if message.startswith(COMMAND_PREFIX):
 				invoked_command = message.split()[0][len(COMMAND_PREFIX)::]
 
-				for command in bot.commands:
+				for command in self.commands:
 					if isinstance(command.COMMAND_NAME, list):  # alias support
 						for alias in command.COMMAND_NAME:
 							if alias == invoked_command:
-								bot.execute_command(command)
+								self.execute_command(command, user, message, channel)
 					else:
 						if command.COMMAND_NAME == invoked_command:
-							bot.execute_command(command)
+							self.execute_command(command, user, message, channel)
+
+	def execute_command(self, cmnd, user, message, channel):
+		if time.time() - cmnd.last_used > cmnd.COOLDOWN:  # cooldown management
+			try:
+				cmnd.execute(self, user, message, channel)
+				log(f"{user} used {COMMAND_PREFIX}{cmnd.COMMAND_NAME} in {channel}")
+				cmnd.last_used = time.time()
+			except Exception as e:
+				error(f"execution of command {cmnd.COMMAND_NAME} failed with {str(e.__class__.__name__)}: {str(e)}")
+				self.send_message(channel, f"{user}, the execution of that command failed! Sorry for the inconvenience, it will be fixed soon hopefully ;w;")
+		else:
+			self.send_message(channel, f"{user}, that command is on cooldown :c")
+
+	def start(self):
+		self.connect()
+
+		while True:
+			try:
+				response = self.socket.recv(2048).decode("utf-8", "ignore")
+				if not response:
+					error("Disconnected from Twitch. Reconnecting...")
+					self.reconnect()
+					continue
+			except Exception as e:
+				error(f"Failed to recv from Twitch, exception: {e.__class__.__name__}")
+				error(f"Printing traceback: ")
+				traceback.print_exc()
+
+				log("Attempting reconnection.")
+				self.reconnect()
+				continue
+
+			self.process_irc_message(response)
+
+
+
+if __name__ == "__main__":
+	bot = kawaiibotto()
