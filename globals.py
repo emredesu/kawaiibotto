62bd404e4b86f1e316d2eaa8610768842a52cc4f
emredesu <ahmetemrebenek@gmail.com>
2021-06-05 20:43:11 +0300

    add TWITCH_DELIMETER variable

--- globals.py
+++ globals.py
@@ -1,14 +1,15 @@
-OAUTH_TOKEN = ""
-USERNAME = ""
-CLIENT_ID = ""
+OAUTH_TOKEN = "bb0tt4vp1l1h093uqgkex992enq3jm"
+USERNAME = "kawaiibotto"
+CLIENT_ID = "g5zg0400k4vhrx2g6xi4hgveruamlv"
 TWITCH_API_HEADERS = {"Authorization": "Bearer {}".format(OAUTH_TOKEN), "Client-ID": "{}".format(CLIENT_ID)}
-WOLFRAM_APP_ID = ""
+WOLFRAM_APP_ID = "947PTK-K6L9T6XA7P"
 COMMAND_PREFIX = "_"
+TWITCH_DELIMITER = "\r\n"
 
 
-channels = []
+channels = ["emredesu", "aysvddgzh23t623q", "alzexx"]
 
-debug_channel = ""
+debug_channel = "i_am_a_terrible_person"
 channels.append(debug_channel)
 
 HOST = "irc.chat.twitch.tv"
