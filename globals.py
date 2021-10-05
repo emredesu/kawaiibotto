f7560dceff1737b346d6380bf853676d2c139bb1
emredesu <ahmetemrebenek@gmail.com>
2021-10-05 21:19:20 +0300

    Added a new key for the new OCR API.

--- globals.py
+++ globals.py
@@ -2,9 +2,10 @@ OAUTH_TOKEN = ""
 USERNAME = ""
 CLIENT_ID = ""
 TWITCH_API_HEADERS = {"Authorization": "Bearer {}".format(OAUTH_TOKEN), "Client-ID": "{}".format(CLIENT_ID)}
-WOLFRAM_APP_ID = ""
+WOLFRAM_APP_ID = "947PTKK6L9T6XA7P"
 COMMAND_PREFIX = "_"
 TWITCH_DELIMITER = "\r\n"
+OCR_SPACE_APIKEY = ""
 
 
 channels = []
