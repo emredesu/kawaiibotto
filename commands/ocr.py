d7e7625aed085d32ffd31d87f16c5e259c1f119d
emredesu <ahmetemrebenek@gmail.com>
2023-04-24 23:44:27 +0300

    automatic conversion of full language names and two letter language codes

--- commands/ocr.py
+++ commands/ocr.py
@@ -4,7 +4,7 @@ import requests
 
 class OCRCommand(Command):
     COMMAND_NAME = "ocr"
-    COOLDOWN = 30
+    COOLDOWN = 10
     DESCRIPTION = "Use OCR (optical character recognition) to extract the text from an image link. You can specify the language" \
                 "using \"lang:(language code here)\". Example usage: _ocr https://i.nuuls.com/leMKr.png lang:jpn"
 
@@ -17,6 +17,63 @@ class OCRCommand(Command):
             if arg.startswith("lang:"):
                 target_language = arg[5:]
                 message_args.remove(arg)
+
+        validLanguageCodes = ["ara", "bul", "chs", "cht", "hrv", "cze", "dan", "dut", "eng", "fin", "fre", "ger", "gre", "hun", "kor", "ita", "jpn", "pol", "por", "rus", "slv", "spa", "swe", "tur"]
+
+        # A dictionary that converts full language names and two letter codes into three letter codes.
+        matchedLanguageCodes = {
+            "arabic": "ara",
+            "ar": "ara",
+            "bulgarian": "bul",
+            "bg": "bul",
+            "chinese": "chs",
+            "ch": "chs",
+            "chinesetraditional": "cht",
+            "crotian": "hrv",
+            "hr": "hrv",
+            "czech": "cze",
+            "cz": "cze",
+            "danish": "dan",
+            "da": "dan",
+            "dutch": "dut",
+            "nl": "dut",
+            "english": "eng",
+            "en": "eng",
+            "finnish": "fin",
+            "fi": "fin",
+            "german": "ger",
+            "de": "ger",
+            "hungarian": "hun",
+            "hu": "hun",
+            "korean": "kor",
+            "ko": "kor",
+            "italian": "ita",
+            "it": "ita",
+            "japanese": "jpn",
+            "ja": "jpn",
+            "polish": "pol",
+            "pl": "pol",
+            "portuguese": "por",
+            "pt": "por",
+            "russian": "rus",
+            "ru": "rus",
+            "slovenian": "slv",
+            "sl": "slv",
+            "spanish": "spa",
+            "es": "spa",
+            "swedish": "swe",
+            "sv": "swe",
+            "turkish": "tur",
+            "tr": "tur"
+        }
+
+        # Check if the user has inputted a valid language code. If not, try to match it with a commonly used language code. 
+        if target_language not in validLanguageCodes:
+            try:
+                target_language = matchedLanguageCodes[target_language]
+            except KeyError:
+                bot.send_message(channel, f"{user}, the language code you inputted is invalid. Valid codes can be found at: https://ocr.space/OCRAPI#:~:text=faster%20upload%20speeds.-,language,-%5BOptional%5D%0AArabic")
+                return
 
         targetRequest = f"https://api.ocr.space/parse/imageurl?apikey={OCR_SPACE_APIKEY}&url={message_args[0]}"
         if target_language != "eng":
