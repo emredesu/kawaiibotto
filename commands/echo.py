a1d51c04ee30b9cf4aa780d7564f02c70ed60f41
emredesu <ahmetemrebenek@gmail.com>
2024-07-25 00:20:11 +0300

    first whisper command

--- commands/echo.py
+++ commands/echo.py
@@ -1,4 +1,4 @@
-from commands.command import Command
+from commands.command import Command, WhisperComand
 from globals import AUTHORIZED_USER
 
 class EchoCommand(Command):
@@ -22,3 +22,27 @@ class EchoCommand(Command):
                 break
 
         bot.send_message(targetChannel, " ".join(args))
+    
+class EchoWhisperCommand(Command, WhisperComand):
+    COMMAND_NAME = "echo"
+    COOLDOWN = 0
+
+    def execute(self, bot, messageData):
+        if messageData.whisperUser != AUTHORIZED_USER:
+            return
+        
+        targetChannel = ""
+
+        args = messageData.whisperContent.split()
+        args.pop(0)
+
+        for arg in args:
+            if arg.startswith("ch:"):
+                targetChannel = arg[3::]
+                args.remove(arg)
+                break
+
+        if targetChannel == "":
+            bot.send_whisper(messageData, "You didn't specify a channel!")
+        else:
+            bot.send_message(targetChannel, " ".join(args))
