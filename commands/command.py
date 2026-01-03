6b89c366b0e09e69d511a267a1b29803a805c978
emredesu <ahmetemrebenek@gmail.com>
2026-01-03 18:15:22 +0300

    add support for custom commands

--- commands/command.py
+++ commands/command.py
@@ -23,3 +23,14 @@ class WhisperComand:
 
 	def execute(self, bot, messageData):
 		pass
+
+# This command type can be triggered through various conditions (e.g. keywords, random chance etc.). This command type also only
+# handles messages received on pre-determined channels.
+class CustomCommand:
+	def __init__(self, commands):
+		commands.append(self)
+
+	CHANNELS = []
+
+	def HandleMessage(self, bot, messageData):
+		pass
