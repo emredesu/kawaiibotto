d357fdb01ed6e642a86967581eb0853c4b2c994b
emredesu <ahmetemrebenek@gmail.com>
2026-01-28 23:10:58 +0300

    add a cooldown to :3

--- commands/quoteResponds.py
+++ commands/quoteResponds.py
@@ -1,10 +1,14 @@
 from commands.command import CustomCommand
+import time
 
 class QuoteRespondsCommand(CustomCommand):
     colonThreeChannel = ""
+    colonThreeCooldown = 10
+    colonThreeLastTriggerTime = 0
 
     CHANNELS = [colonThreeChannel]
 
     def HandleMessage(self, bot, messageData):
-        if messageData.channel.lower() == self.colonThreeChannel and messageData.content == ":3":
+        if (messageData.channel.lower() == self.colonThreeChannel and messageData.content == ":3") and self.colonThreeLastTriggerTime + self.colonThreeCooldown < time.time():
             bot.send_message(messageData.channel, ":3")
+            self.colonThreeLastTriggerTime = time.time()
